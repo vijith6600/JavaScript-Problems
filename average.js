acdf1dc3f4c3df5eee5af5140a246736b8d449f4
vijith6600 <vijith4y@gmail.com>
2024-02-24 15:19:24 +0530

    javascript problems

---
+++ average.js
@@ -0,0 +1,81 @@
+// average of 5 subject
+
+// var maths = 20
+// var english = 40
+// var hindi = 70
+// var tamil = 90
+
+// const averageMark = Math.floor(maths+english+hindi+tamil / 4);
+// console.log(averageMark);
+
+
+
+
+// // Concantinate 2 string
+
+// var firstName="John"
+// var LastName="Doe"
+
+// console.log(`${firstName} ${LastName}`);
+
+
+
+// // find the reminder divided by 5
+
+// var num = 166
+
+// console.log(`The reminder  when ${num} is divided by 5 is = ${num % 5}`);
+
+
+
+
+// // You are given an array:
+
+// var fruits = ['Apple', 'Banana', 'Orange'];
+
+// // a) Find the index of ‘Banana’ and replace ‘Banana’ with ‘Mango’.
+// // b) Remove ‘Orange’ and add ‘Watermelon’.
+
+//  console.log(fruits.indexOf('banana'));
+
+
+
+
+
+// // find largest number
+
+// a=10
+// b=9
+// c=2
+
+// if(a>b && a>c){
+//     console.log("Largest Number is "+a)
+// }
+// else if(b>c){
+
+//     console.log("Largest Number is "+b)
+// }
+// else{
+//     console.log("largest number is "+c);
+// }
+
+
+
+
+// check the traingle is isosuless or not
+
+side1 =80
+side2=80
+side3=80
+
+if(side1==side2 && side1==side3 && side2==side3){
+    console.log(" is a equalatral trangle");
+}
+else if(side1==side2 || side1==side3 || side2==side3){
+    console.log("trangle is isosuless");
+}
+
+else{console.log("Traingle is not ispsulus");}
+
+
+
