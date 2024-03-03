f68cb1238321c9bb8a8ac56a968877da7be16bb1
vijith6600 <vijith4y@gmail.com>
2024-03-03 21:36:39 +0530

    lcm

---
+++ Lcm.js
@@ -0,0 +1,17 @@
+// num1 = 4
+// num2 = 6
+
+
+// function lcm(num1,num2){
+//     const max = Math.max(num1, num2)
+//     const min = Math.min(num1,num2)
+    
+//    for(i = max;;i+=max){
+//         i%min==0;
+//         return i;
+//     }
+// }
+
+// console.log(lcm(num1,num2));
+
+
