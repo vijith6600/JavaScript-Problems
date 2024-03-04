2fd00e980e43100506dded669b5cc6df5b1d4669
vijith6600 <vijith4y@gmail.com>
2024-03-04 12:41:01 +0530

    pattern

---
+++ Pattern1.js
@@ -0,0 +1,18 @@
+// print pattern
+
+// 1111
+// 2222
+// 3333
+// 4444
+
+
+for(row=1; row<=4; row++){
+    str=""
+    for(col=1; col<=4; col++){
+        str+=row
+
+    }
+    console.log(str);
+
+}
+
