f27e9b1bef25634b311d9af15763524a0495522f
vijith6600 <vijith4y@gmail.com>
2024-03-04 12:53:35 +0530

    pattern problem 3 finished

---
+++ Pattern2.js
@@ -0,0 +1,13 @@
+// print pattern 
+
+// # # # #
+// # # # #
+// # # # #
+
+for(row=1; row<=4; row++){
+    pattern=""
+    for(col=1; col<=4; col++){
+        pattern+= "#"
+    }
+    console.log(pattern);
+}
