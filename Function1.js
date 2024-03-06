617d3487991e37df4e07c78ceb33ef8b40411fe9
vijith6600 <vijith4y@gmail.com>
2024-03-06 23:38:05 +0530

    function related

---
+++ Function1.js
@@ -0,0 +1,15 @@
+// write js functio to reverse astring
+
+// function reverse(str){
+//     let reverseString=""
+//     for(let i=str.length-1; i>=0; i--){
+//         reverseString+= str[i]
+//     }
+//     return console.log(reverseString);
+// }
+// reverse("hello")
+
+
+// wite a function to reverse a number
+
+
