62275040099d049fa5b771b94cdfc79c3146e815
vijith6600 <vijith4y@gmail.com>
2024-03-09 22:28:39 +0530

    push

---
+++ Function2.js
@@ -0,0 +1,12 @@
+// Write a javascript function to Cjeck the word start with Caps or not
+
+
+function checkCaps(word){
+    if(word[0]=="A"){
+        console.log("caps");
+    }else{
+        console.log("Not caps");
+    }
+}
+
+checkCaps()
