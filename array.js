ab52e61e458dc06e749a7cd0b0e132b611b1a627
vijith6600 <vijith4y@gmail.com>
2024-03-19 18:03:56 +0530

    array

--- array.js
+++ array.js
@@ -1,9 +1,18 @@
-//array length
+// //array length
+// var car = ["toyota" , "bmw" , "audi"]
+
+// console.log(car.length-1);
+
+// let arr = new Array
+// console.log(car);
+// arr.push("mango","apple","orange","banana")
+// console.log(arr);
+
+
+// //display value stored in array ("of" is used)
 var car = ["toyota" , "bmw" , "audi"]
 
-console.log(car.length-1);
+for(item of car){
+    console.log(item);
+}
 
-let arr = new Array
-console.log(car);
-arr.push("mango","apple","orange","banana")
-console.log(arr);
